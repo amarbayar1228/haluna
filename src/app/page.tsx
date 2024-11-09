fe173f36b443cc160bc570ffe7c30c07d9bbc908
abbuyanaa <abbuyanaa@gmail.com>
2024-11-09 15:53:13 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ interface CountdownTimerProps {
 }
 export default function Home() {
 
-  const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate }) => {
     const [timeLeft, setTimeLeft] = useState({
       days: 0,
       hours: 0,
@@ -21,7 +20,7 @@ export default function Home() {
       // Function to update time left
       const updateTime = () => {
         const now: any = new Date();
-        const target: any = new Date(targetDate);
+        const target: any = new Date("2024-11-13:20:00:00");
 
         const difference = target - now;
 
@@ -46,14 +45,23 @@ export default function Home() {
 
       // Clean up interval on component unmount
       return () => clearInterval(interval);
-    }, [targetDate]);
+    }, [timeLeft]);
+ 
 
-    return (
-      <div className="bg-[#ddd1ca] pt-5 pb-20 relative">
+  const targetDate = '2024-11-13:20:00:00'; // Ensure the date is in the correct format (ISO 8601)
+ 
+
+  return (
+    <div className="flex justify-center w-full ">
+      <div className="w-[700px] max-md:w-full text-xs">
         
+        <Image alt="zurag" className="h-[80vh]" src="/image/white1.png" width={1000} height={8000} />
+        {/* Show countdown timer */}
+        <div className="bg-[#ddd1ca] pt-5 pb-20 relative">
         <div className="flex justify-center gap-2 text-[#735c4f] font-bold text-3xl pb-5 relative">
         <div className="w-[73%] mx-auto mt-5 h-[1px] bg-white absolute z-0"></div>
           <div className="bg-[#ddd1ca] z-10 px-3">Party Event</div>
+          
         </div>
         <div className="flex justify-center gap-2 text-[#735c4f] capitalize">
           <span className="border text-center p-4 flex-col">
@@ -73,36 +81,7 @@ export default function Home() {
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25547.426646652788!2d127.12546439011071!3d36.83221653736704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b28637d4ed659%3A0xec0460c3f536f0e5!2z7Lap7LKt64Ko64-EIOyynOyViOyLnCDshJzrtoHqtawg7ISx7KCV64-ZIDE1MDY!5e0!3m2!1sko!2skr!4v1731132951220!5m2!1sko!2skr" 
         width="320" height="300" loading="lazy"></iframe>
         </div>
-        {/* Gradient background div */}
-        {/* <div style={{ 
-          background: "linear-gradient(214deg, #E05F8A 0%, #E3AABD 100%)",
-          backgroundColor: "transparent", // Optional, if you want to allow the gradient to show through a transparent background
-          height: "400px",
-          borderRadius: "8px", // Optional for rounded corners
-          border: "0.5px solid #fff",
-          margin: "5px 10px"
-        }}>
-          <div style={{
-            backgroundImage: "url(/Picture1.png)",
-            backgroundPosition: "center center", 
-            backgroundSize: "cover",
-            zIndex: 1
-          }}>
-            <div style={{position: "relative", zIndex: 2}}>asdasd</div>
-          </div> 
-        </div> */}
       </div>
-    );
-  };
-
-  const targetDate = '2024-11-13:20:00:00'; // Ensure the date is in the correct format (ISO 8601)
-
-  return (
-    <div className="flex justify-center w-full ">
-      <div className="w-[700px] max-md:w-full text-xs">
-        <Image alt="zurag" className="h-[80vh]" src="/image/white1.png" width={1000} height={8000} />
-        {/* Show countdown timer */}
-        <CountdownTimer targetDate={targetDate} />
         {/* Text section with a custom font */}
       </div>
       <FloatButton.BackTop />
